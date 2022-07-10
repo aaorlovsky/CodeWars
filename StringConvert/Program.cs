24157181088942cf771bb5df3e41ba24e16207aa
Alexander Orlovsky <aaorlovsky@hotmail.com>
2022-07-10 15:32:52 +0300

    Best practice solution for String Count

--- StringConvert/Program.cs
+++ StringConvert/Program.cs
@@ -4,5 +4,6 @@ string val = Console.ReadLine()!;
 
 Console.WriteLine(new string(val.ToLower().Select(ch => val.ToLower().Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray()));
 
+Console.WriteLine(val.ToLower().GroupBy(ch => ch).Where(g => g.Count() > 1).Count());
 
 
