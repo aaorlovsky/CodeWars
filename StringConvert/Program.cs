ee1f5e24869e60912565acd4366db60ffd6bdee3
Alexander Orlovsky <aaorlovsky@hotmail.com>
2022-07-11 22:39:27 +0300

    Adding FindMissing method

--- StringConvert/Program.cs
+++ StringConvert/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System.Linq;
 
-string val = Console.ReadLine()!;
+//string val = Console.ReadLine()!;
 
-Console.WriteLine(new string(val.ToLower().Select(ch => val.ToLower().Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray()));
+//Console.WriteLine(new string(val.ToLower().Select(ch => val.ToLower().Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray()));
 
-Console.WriteLine(val.ToLower().GroupBy(ch => ch).Where(g => g.Count() > 1).Count());
+//Console.WriteLine(val.ToLower().GroupBy(ch => ch).Where(g => g.Count() > 1).Count());
+
+
+Console.WriteLine(FindMissingLetter(new char[] { 'O', 'Q', 'R', 'S' }));
+
+static char FindMissingLetter(char[] array)
+{
+    for (var i = 0; i < array.Count() - 1; i++)
+    {
+        if ((int)array[i + 1] - (int)array[i] > 1)
+        {
+            return (char)((int)array[i] + 1);
+        }
+
+    }
+    return ' ';
+}
 
 
